9c3d5e759328769861e6c23d88cbd1fb1468cbf1
James Gayfer <james@stembolt.com>
2018-08-03 13:37:15 -0700

    Add count state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
   render() {
     return (
       <div>
-        <div>45</div>
+        <div>{this.state.count}</div>
         <button>-</button>
         <button>reset</button>
         <button>+</button>
