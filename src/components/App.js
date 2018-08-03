19519e7e0c3f7838cae3dda2a1c7e9214e03cad6
James Gayfer <james@stembolt.com>
2018-08-03 14:05:52 -0700

    Add Counter component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import Counter from './Counter';
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -25,7 +27,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <div>{this.state.count}</div>
+        <Counter { ...this.state } />
         <button onClick={ () => this.decrementCount() }>-</button>
         <button onClick={ () => this.resetCount() }>reset</button>
         <button onClick={ () => this.incrementCount() }>+</button>
